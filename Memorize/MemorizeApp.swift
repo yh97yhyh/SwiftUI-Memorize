3498ae04ab2faca1669ce95845e0fc0a90925040
MZS01-KYOUNGH <mzs01-kyoungh@MZS01-KYOUNGH.local>
2023-04-14 14:33:37 +0900

    Lecture2: Learning More about SwiftUI

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by MZS01-KYOUNGH on 2023/04/14.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
